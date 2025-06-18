3277b0eba4953a813f2d70483d334729dce8facb
rayed152 <rayed152@gmail.com>
2025-06-18 16:55:53 +0600

    Changed game page name to create-team

--- app/create-team/page.tsx
+++ app/create-team/page.tsx
@@ -1,6 +1,6 @@
 import CreateTeam from "@/components/game-components/create-team";
 
-export default function Game() {
+export default function TeamCreate() {
   return (
     <div className="">
       <CreateTeam />
